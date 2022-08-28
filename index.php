38d6aac8fc138f07ad768be0e0b7a926f4b61503
Anita <neerusharma.anita@gmail.com>
2022-08-28 17:12:53 +0800

    Contact form project

--- index.php
+++ index.php
@@ -3,23 +3,71 @@
   <head>
     <meta charset="utf-8">
     <title>Contact Form</title>
+	
     <link rel="stylesheet" href="style.css"/>
   </head>
-  <body>
-    
-    <form id="form">
-      <label for="email">Email:</label>
-      <input type="text" name="email" id="email" placeholder="email@example.com"><br>
-      <label for="subject">Subject:</label>
-      <input type="text" name="subject" id="subject" placeholder="subject"><br>
-      <label for="message">Message:</label>
-      <textarea name="message" id="message" placeholder="message"></textarea><br>
+  
+<body>
+
+
+<div class="container">
+<div class="center">
+
+<h2>Contact Form</h2>
+ <form id="form">
+    <div class="row">
+      <div class="col-25">
+        <label for="fname">First Name</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="fname" name="firstname" placeholder="Your first name..">
+      </div>
+    </div>
+	<div class="row">
+      <div class="col-25">
+        <label for="fname">Last Name</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+         <label for="email">Email:</label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="email" id="email" placeholder="email@example.com">
+      </div>
+    </div>
+   
+    <div class="row">
+      <div class="col-25">
+       <label for="message">Message:</label>
+      </div>
+      <div class="col-75">
+        <textarea name="message" id="message" placeholder="Write something.." style="height:200px"></textarea>
+      </div>
+    </div>
+	<div class="row">
+      <div class="col-25">
+       	<label>Your Image File</label>
+  
+      </div>
+      <div class="col-75">
+        <input type="file" name="myImage" id="myImage" accept="image/png, image/gif, image/jpeg"/>
+      </div>
+    </div>
+
+
+    <div class="row">
       <button name="submit" id="submit">Send</button>
       <label id="loader"></label>
       <label id="info"></label>
-    </form>
-    
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    </div>
+  </form>
+  </div>
+</div>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="main.js"></script>
-  </body>
+</body>
 </html>
--- main.js
+++ main.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
 
   var form = $('#form'),
       email = $('#email'),
-      subject = $('#subject'),
-      message = $('#message'),
+      fname = $('#fname'),
+	  lname = $('#lname'),
+	  message = $('#message'),
+	  myImage = $('#myImage'),
       info = $('#info'),
       loader = $('#loader'),
       submit = $("#submit");
   
-  form.on('input', '#email, #subject, #message', function() {
+  form.on('input', '#email, #fname, #lname, #message, #myImage', function() {
     $(this).css('border-color', '');
     info.html('').slideUp();
   });
@@ -25,8 +27,10 @@ $(document).ready(function() {
       }).done(function(data) {
         if(data.success) {
           email.val('');
-          subject.val('');
+		  fname.val('');
+          lname.val('');
           message.val('');
+		  myImage.val('');
           info.html('Message sent!').css('color', 'green').slideDown();
         } else {
           info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
@@ -43,16 +47,41 @@ $(document).ready(function() {
       email.css('border-color', 'red');
       valid = false;
     }
-    if($.trim(subject.val()) === "") {
-      subject.css('border-color', 'red');
+    if($.trim(fname.val()) === "") {
+      fname.css('border-color', 'red');
+      valid = false;
+    }
+	if($.trim(lname.val()) === "") {
+      lname.css('border-color', 'red');
       valid = false;
     }
     if($.trim(message.val()) === "") {
       message.css('border-color', 'red');
       valid = false;
     }
+	if($.trim(myImage.val()) === "") {
+      myImage.css('border-color', 'red');
+      valid = false;
+    }
     
     return valid;
   }
+  function manage(txt) {
+        var bt = document.getElementById('submit');
+        var ele = document.getElementsByTagName('input'); 
+
+        // Loop through each element.
+        for (i = 0; i < ele.length; i++) {
+
+            // Check the element type.
+            if (ele[i].type == 'text' && ele[i].value == '') {
+                bt.disabled = true;    // Disable the button.
+                return false;
+            }
+            else {
+                bt.disabled = false;   // Enable the button.
+            }
+        }
+    }
 
 });
